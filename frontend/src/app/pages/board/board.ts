565072d0c9c44b00b385bde095eb8f1187fa8aaf
zielonyogor <kasia.jaromirska@gmail.com>
2026-04-04 00:16:34 +0200

    Add user story card

--- frontend/src/app/pages/board/board.ts
+++ frontend/src/app/pages/board/board.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 import { DatePipe } from '@angular/common';
-import { Sprint } from '../../../types/sprint';
-import { UserStory } from '../../../types/userStory';
+import type { Sprint } from '../../../types/sprint';
+import { USER_STORY_STATUSES, UserStoryStatus, type UserStory } from '../../../types/userStory';
+import { UserStoryCard } from './components/user-story-card/user-story-card';
 
 @Component({
   selector: 'app-board',
   templateUrl: './board.html',
   styleUrl: './board.scss',
-  imports: [DatePipe],
+  imports: [DatePipe, UserStoryCard],
 })
 export class Board {
+  protected readonly columns = USER_STORY_STATUSES;
+
   protected readonly sprint: Sprint = {
       id: '1',
       goal: 'Make MVP',
@@ -31,10 +34,22 @@ export class Board {
       description: 'Description for User Story 2',
       status: 'In Progress',
       sprintId: '1'
+    },
+    {
+      id: '3',
+      title: 'User Story 3',
+      description: 'Description for User Story 3. But this time this is a bit longer of a description heh',
+      status: 'Code Review',
+      sprintId: '1'
     }
-  ]
+  ];
 
-  protected getUserStoriesForColumn(status: 'To Do' | 'In Progress' | 'Done'): UserStory[] {
+  protected getUserStoriesForColumn(status: UserStoryStatus): UserStory[] {
     return this.userStories.filter(story => story.status === status);
+  }
+
+  protected onOpenStory(story: UserStory): void {
+    // Open user story modal here
+    console.log('Open user story', story.id);
   }
 }
--- frontend/src/types/userStory.ts
+++ frontend/src/types/userStory.ts
@@ -1,7 +1,12 @@
+export const USER_STORY_STATUSES = ['To Do', 'Blocked','In Progress', 'Code Review', 'Done'] as const;
+
+export type UserStoryStatus = (typeof USER_STORY_STATUSES)[number];
+
+
 export interface UserStory {
     id: string;
     title: string;
     description: string;
-    status: 'To Do' | 'In Progress' | 'Done';
+    status: UserStoryStatus;
     sprintId: string;
 }
