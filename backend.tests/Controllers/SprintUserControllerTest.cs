0911b71d2bd6c524a7a86af4ce65af4d6106cf21
MateuszN949 <mateusz.nowicki@onet.eu>
2026-05-10 23:24:54 +0200

    Add tests for sprint users

---
+++ backend.tests/Controllers/SprintUserControllerTest.cs
@@ -0,0 +1,143 @@
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SprintTracker.Controllers;
+using SprintTracker.Database.Data;
+using SprintTracker.Database.Models;
+using SprintTracker.DTO.Requests;
+
+namespace SprintTracker.Tests.Controllers
+{
+    public class SprintUserControllerTests
+    {
+        [Fact]
+        public void CreateUser_ShouldReturnCreatedAndPersistUser()
+        {
+            using var context = GetDatabaseContext();
+            var controller = new SprintUserController(context);
+
+            var result = controller.CreateUser(new CreateUserRequest
+            {
+                Name = "Developer"
+            });
+
+            var createdResult = result.Should().BeOfType<CreatedAtActionResult>().Subject;
+            context.Users.Should().Contain(u => u.Name == "Developer");
+            createdResult.RouteValues.Should().ContainKey("id");
+        }
+
+        [Fact]
+        public void CreateUser_ShouldReturnBadRequest_WhenNameIsEmpty()
+        {
+            using var context = GetDatabaseContext();
+            var controller = new SprintUserController(context);
+
+            var result = controller.CreateUser(new CreateUserRequest
+            {
+                Name = " "
+            });
+
+            result.Should().BeOfType<BadRequestObjectResult>();
+        }
+
+        [Fact]
+        public void JoinSprint_ShouldReturnOkAndAddSprintMember_WhenUserAndSprintExist()
+        {
+            using var context = GetDatabaseContext();
+            SeedSprints(context);
+            SeedUsers(context);
+
+            var controller = new SprintUserController(context);
+
+            var result = controller.JoinSprint(1, new JoinSprintRequest
+            {
+                UserId = 1
+            });
+
+            result.Should().BeOfType<OkObjectResult>();
+            context.SprintMembers.Should().Contain(sm => sm.SprintId == 1 && sm.UserId == 1);
+        }
+
+        [Fact]
+        public void JoinSprint_ShouldReturnBadRequest_WhenUserDoesNotExist()
+        {
+            using var context = GetDatabaseContext();
+            SeedSprints(context);
+
+            var controller = new SprintUserController(context);
+
+            var result = controller.JoinSprint(1, new JoinSprintRequest
+            {
+                UserId = 99
+            });
+
+            result.Should().BeOfType<BadRequestObjectResult>();
+        }
+
+        [Fact]
+        public void JoinSprint_ShouldReturnNotFound_WhenSprintDoesNotExist()
+        {
+            using var context = GetDatabaseContext();
+            SeedUsers(context);
+
+            var controller = new SprintUserController(context);
+
+            var result = controller.JoinSprint(1, new JoinSprintRequest
+            {
+                UserId = 1
+            });
+
+            result.Should().BeOfType<NotFoundObjectResult>();
+        }
+
+        [Fact]
+        public void GetSprintMembers_ShouldReturnOk_WithMemberList()
+        {
+            using var context = GetDatabaseContext();
+            SeedSprints(context);
+            SeedUsers(context);
+            SeedSprintMembers(context);
+
+            var controller = new SprintUserController(context);
+
+            var result = controller.GetSprintMembers(1);
+
+            var okResult = result.Should().BeOfType<OkObjectResult>().Subject;
+            var members = okResult.Value.Should().BeAssignableTo<IEnumerable<object>>().Subject;
+            members.Should().HaveCount(2);
+        }
+
+        private AppDbContext GetDatabaseContext()
+        {
+            var options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            var context = new AppDbContext(options);
+            context.Database.EnsureCreated();
+            return context;
+        }
+
+        private void SeedSprints(AppDbContext context)
+        {
+            context.Sprints.Add(new Sprint { Id = 1, Name = "Sprint 1" });
+            context.SaveChanges();
+        }
+
+        private void SeedUsers(AppDbContext context)
+        {
+            context.Users.Add(new User { Id = 1, Name = "Alice" });
+            context.Users.Add(new User { Id = 2, Name = "Bob" });
+            context.SaveChanges();
+        }
+
+        private void SeedSprintMembers(AppDbContext context)
+        {
+            context.SprintMembers.Add(new SprintMember { Id = 1, SprintId = 1, UserId = 1 });
+            context.SprintMembers.Add(new SprintMember { Id = 2, SprintId = 1, UserId = 2 });
+            context.SaveChanges();
+        }
+    }
+}
