264782be00e7ace54a23e12a2a6698f87e20a13f
MateuszN949 <mateusz.nowicki@onet.eu>
2026-05-10 21:24:49 +0200

    Add db migration to container

--- backend/Program.cs
+++ backend/Program.cs
@@ -52,6 +52,12 @@ namespace SprintTracker
 
             app.MapControllers();
 
+            using (var scope = app.Services.CreateScope())
+            {
+                var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                db.Database.Migrate();
+            }
+
             app.Run();
         }
     }
